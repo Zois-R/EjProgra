b29f68451176678e0459c7a5444acfd26a3180df
Yerrba <zoisruggiero@hotmail.com>
2022-08-02 12:24:00 -0300

    main

---
+++ main.c
@@ -0,0 +1,41 @@
+#include "funciones.h"
+
+int main()
+{
+
+
+    tFecha fecha1 = {2,5,2019};
+    //printf("%d\n",esFechaValida(&fecha1));
+    sumarNdiasFecha(&fecha1,10);
+    return 0;
+}
+
+
+/*
+system("pause");
+system("cls");
+*/
+
+
+///    LOTE DE PRUEBA PARA EJERCICIO N°1 DE FACTORIAL
+//    int n = 7;
+//    int guardo;
+//    guardo = factorial(n);
+//    printf("El factorial de %d es %d",n,guardo);
+
+
+
+
+/// LOTE DE PRUEBA PARA EJERCICIO N°2 DE NUMERO COMBINATORIO
+//    int n=9;
+//    int m = 13;
+//    int guardo;
+//    guardo = combinatorio(n,m);
+//    printf("COMBINATORIO ES %d",guardo);
+
+
+
+///LOTE DE PRUEBA EJ 15 SUMO UNO A LA FECHA
+//    tFecha fecha1 = {31,5,2020};
+//    printf("%d\n",esFechaValida(&fecha1));
+//    sumoUnDiaFecha(&fecha1);
